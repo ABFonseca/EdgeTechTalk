6c018cab3562795fdbf1f9abe08a5adbde04479b
António Fonseca <antonio.v.fonseca@galp.com>
2020-04-16 15:42:13 +0100

    Added advances DB example

--- api-bd2.py
+++ api-bd2.py
@@ -9,7 +9,7 @@ from jwt import encode, decode, ExpiredSignatureError, InvalidTokenError
 from functools import wraps
 import datetime
 
-import database as db
+import database2 as db
 
 
 
@@ -77,29 +77,24 @@ def token_required(f):
 @APP.route('/user_info', methods=['GET'])
 def user_list():
     user_list = [{'id': u.u_id, 'username': u.username} for u in db.get_users()] #CHANGE
-    print(user_list)
     response = jsonify({'users': user_list})
     return response, 200
         
 
 
-@APP.route('/user_info/<int:user_id>', methods=['GET', 'POST'])
-@token_required
+@APP.route('/movies', methods=['GET'])
+def movie_list():
+    movie_list = [{'id': m.m_id, 'name': m.name} for m in db.get_movies()] 
+    response = jsonify({'movies': movie_list})
+    return response, 200
+        
+
+
+@APP.route('/user_info/<int:user_id>', methods=['GET'])
 def user(user_id):
     def get_user_info(user):
-        response = jsonify({'user': user.username, 'movie': user.favorite_movie})
+        response = jsonify({'user': user.username})
         return response
-
-    def post_user_info(user):
-        try:
-            req_json = request.get_json()
-            new_favorite = req_json['movie']
-            user.favorite_movie = new_favorite
-            response = jsonify({'status': 'success'})
-            return response
-        except:
-            response = jsonify({'status': 'failed', 'fail reason': 'Wrong arguments'})
-            return response, 500
         
     user = db.get_user_by_id(user_id) #CHANGE
     if user is None:
@@ -107,13 +102,48 @@ def user(user_id):
         return response, 404
     if request.method == 'GET':
         return get_user_info(user)
-    elif request.method == 'POST':
-        return post_user_info(user)
     else:
         response = jsonify({'status': 'failed', 'fail reason': 'Wrong method, only POST and GET available'})
         return response, 401
-        
-        
+
+
+@APP.route('/user_info/<int:user_id>/movies', methods=['GET', 'POST'])
+def user_movies(user_id):
+    def get_user_movies(u_id):
+        results = db.get_movies_for_user(user_id)
+        if not results:
+            response = jsonify({'Movies': []})
+        else:
+            response = jsonify({'Movies': [{'movieID': movie.m_id, 'movieName': movie.name} for movie in results]})
+        return response, 200
+    def new_user_movie(u_id):
+        try:
+            req_json = request.get_json()
+            new_favorite = req_json['movie']
+            movie = db.get_movie_by_name(new_favorite)
+            if not movie:
+                return jsonify({'status': 'failed', 'fail reason': 'Movie not found'})
+            db.create_user_movie(u_id, movie.m_id)
+            response = jsonify({'status': 'success'})
+            return response
+        except:
+            response = jsonify({'status': 'failed', 'fail reason': 'Wrong arguments'})
+            return response, 500
+    
+    if request.method == 'GET':
+        return get_user_movies(user_id)
+    elif request.method == 'POST':
+        return new_user_movie(user_id)
+    else:
+        response = jsonify({'status': 'failed', 'fail reason': 'Wrong method, only POST and GET available'})
+        return response, 401
+
+
+@APP.route('/user_info/<int:user_id>/movies/<int:movie_id>', methods=['GET'])
+def movies_info(user_id, movie_id):
+    movie = db.get_movie_by_id(movie_id)
+    return jsonify({'Name': movie.name, 'Author': movie.author, 'IMDB Rating': movie.imdb_rating})
+
         
 if __name__ == '__main__':
     users = db.get_users() #CHANGE
@@ -121,4 +151,10 @@ if __name__ == '__main__':
         db.create_user('admin_user', 'admin_pass', 'Padrinho')
         db.create_user('readonly_user', 'readonly_pass', 'Jurassic World')
         db.create_user('readwrite_user', 'readwrite_pass', 'Jurassic Park')
+        db.create_movie('Padrinho', 'Mario Puzo', 10)
+        db.create_movie('Padrinho 2', 'Mario Puzo', 9)
+        db.create_movie('Padrinho 3', 'Mario Puzo', 1)
+        db.create_movie('Jurassic Park', 'Michael Crichton', 8)
+        db.create_movie('Jurassic World', 'Derek Connolly, Colin Trevorrow', 0)
+        db.create_movie('Gladiator', 'David Franzoni', 8)
     APP.run(port='5050')
---
+++ database2.py
@@ -0,0 +1,107 @@
+from sqlalchemy import Column, Integer, String, Sequence, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import datetime
+
+engine = create_engine("postgresql://postgres:docker@localhost/api_example")
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'TB_USERS2'
+    u_id = Column(Integer, Sequence('user2_id_seq'), primary_key=True)
+    username = Column(String(100), unique=True)
+    password = Column(String(4000))
+    last_update = Column(DateTime, default=datetime.datetime.now)
+
+    def __init__(self, username, password, fav_movie):
+        self.username = username
+        self.password = password
+        self.favorite_movie = fav_movie
+        
+
+class Movie(Base):
+    __tablename__ = 'TB_MOVIES'
+    m_id = Column(Integer, Sequence('movie_id_seq'), primary_key=True)
+    name = Column(String(100), unique=True)
+    author = Column(String(4000))
+    imdb_rating = Column(Integer)
+
+    def __init__(self, name, author, imdb_rating):
+        self.name = name
+        self.author = author
+        self.imdb_rating = imdb_rating
+		
+
+class UserMovie(Base):
+    __tablename__ = 'TB_USERS_MOVIES'
+    um_id = Column(Integer, Sequence('usermovie_id_seq'), primary_key=True)
+    u_id = Column(Integer, ForeignKey(User.u_id))
+    m_id = Column(Integer, ForeignKey(Movie.m_id))
+
+    def __init__(self, u_id, m_id):
+        self.u_id = u_id
+        self.m_id = m_id
+
+def create_user(name, password, fav_movie):
+    user = User(name, password, fav_movie)
+    session.add(user)
+    session.commit()
+    return user.u_id
+    
+def get_user_by_id(id):
+    result = session.query(User).filter(User.u_id == id).first()
+    return result
+
+def get_user_by_name(name):
+    result = session.query(User).filter(User.username == name).first()
+    return result
+    
+def get_users():
+	results = session.query(User).all()
+	return [result for result in results]
+    
+	
+def create_movie(name, author, imdb_rating):
+	movie = Movie(name, author, imdb_rating)
+	session.add(movie)
+	session.commit()
+	return movie.m_id
+    
+def get_movie_by_id(id):
+    result = session.query(Movie).filter(Movie.m_id == id).first()
+    return result
+
+def get_movie_by_name(name):
+    result = session.query(Movie).filter(Movie.name == name).first()
+    return result
+    
+def get_movies():
+	results = session.query(Movie).all()
+	return [result for result in results]
+
+
+
+def create_user_movie(u_id, m_id):
+	user_movie = UserMovie(u_id, m_id)
+	session.add(user_movie)
+	session.commit()
+	return
+
+def remove_user_movie(u_id, m_id):
+	session.query(UserMovie).filter(UserMovie.u_id == u_id).filter(UserMovie.m_id == m_id).delete()
+	session.commit()
+	return
+	
+def get_movies_for_user(u_id):
+	results = session.query(Movie).join(
+		UserMovie).filter(UserMovie.u_id==u_id).order_by(
+		Movie.imdb_rating).all()
+	return results
+	
+	
+	
+	
+Session = sessionmaker(bind=engine)
+session = Session()
+Base.metadata.create_all(engine)
